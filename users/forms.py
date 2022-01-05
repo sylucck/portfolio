6380d4cd663622557816034d433177ad9e3adc38
sylvester <nkwontasylvester15@gmail.com>
2022-01-05 08:33:33 -0900

    added UserUpdateForm and ProfileUpdateForm, in views.py as well

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import fields
+from .models import *
 #from ecommerce.models import Customer
 
-
+#creating a registration form
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+#creating a user model form
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'image' ]
